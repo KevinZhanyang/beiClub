a219240b516fb01c4a3ada9b99ae3cf0d67defe3
flue fly <cuimmcan@qq.com>
2018-11-03 23:19:27 +0800

    第一次提交

--- config/api.js
+++ config/api.js
@@ -1,4 +1,4 @@
-const ApiRootUrl = 'https://used.beimei2.com';
+const getList = 'https://used.beimei2.com';
 const OSS_ACCESS_KEY_ID = 'LTAIqOUcfWsExRPm';
 const OSS_ACCESS_KEY_SECRET = '2CsV9Qf1QjimVBA3rbQFax31JD77Wg';
 const OSS_ENDPOINT = 'http://oss-us-west-1-internal.aliyuncs.com';
@@ -6,9 +6,10 @@ const OSS_BUCKET = 'used-america';
 const OSS_CDNDOMAIN = 'http://img.beimei2.com';
 const OSS_PROCESS_THUMBNAIL ='? x-oss-process=style/thumbnail';
 const OSS_PROCESS_SMALL_THUMBNAIL ='?x-oss-process=style/small_thumbnail';
+const GET_TAG_LIST=getList+'/used/v1/tag'
 
 module.exports = {
-  
+    GET_TAG_LIST
 };
 
 
---
+++ pages/creat/index.js
@@ -0,0 +1,46 @@
+//index.js
+//获取应用实例
+const app = getApp();
+var uploadImage = require("../../utils/uploadFile.js");
+var util = require("../../utils/util.js");
+import { GET_TAG_LIST } from "../../config/api.js";
+
+Page({
+  data: {
+    tagList: [],
+    selectTag: []
+  },
+  onLoad() {
+    this.getList();
+  },
+  getList() {
+    util.request(GET_TAG_LIST).then(res => {
+      if (res.code == 200) {
+        this.setData({
+          tagList: res.body.tagList
+        });
+      }
+    });
+  },
+  selectTag(event) {
+    let id = event.currentTarget.dataset["id"];
+    let selectTag = this.data.selectTag;
+    let tagList = this.data.tagList;
+    if (selectTag.indexOf(id) > -1) {
+      selectTag.splice(selectTag.indexOf(id), 1);
+    } else {
+      if (selectTag.length > 7) return;
+      selectTag.push(id);
+    }
+    for (let item of tagList) {
+      if (selectTag.indexOf(item.id) > -1) {
+        item.state = 1;
+      } else {
+        item.state = 0;
+      }
+    }
+    this.setData({
+      tagList
+    });
+  }
+});
