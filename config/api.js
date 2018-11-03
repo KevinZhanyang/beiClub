02844ef9de160f3fa80814b13b35cc356e8d7805
zhanyang <zhanyang_work@163.com>
2018-11-03 17:52:19 +0800

    update

--- config/api.js
+++ config/api.js
@@ -1,6 +1,11 @@
-
 const ApiRootUrl = 'https://used.beimei2.com';
-
+const OSS_ACCESS_KEY_ID = 'LTAIqOUcfWsExRPm';
+const OSS_ACCESS_KEY_SECRET = '2CsV9Qf1QjimVBA3rbQFax31JD77Wg';
+const OSS_ENDPOINT = 'http://oss-us-west-1-internal.aliyuncs.com';
+const OSS_BUCKET = 'used-america';
+const OSS_CDNDOMAIN = 'http://img.beimei2.com';
+const OSS_PROCESS_THUMBNAIL ='? x-oss-process=style/thumbnail';
+const OSS_PROCESS_SMALL_THUMBNAIL ='?x-oss-process=style/small_thumbnail';
 
 module.exports = {
   
